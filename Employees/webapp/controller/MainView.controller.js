20d269d1fab178817b27f35f1bad04e708daa691
moizaled <moizaled@gmail.com>
2021-06-16 01:28:42 +0000

    first commit

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,41 @@
+// @ts-nocheck
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup10.Employees.controller.MainView", {
-			onInit: function () {
 
-			}
-		});
-	});
+
+        var Main = Controller.extend("logaligroup10.Employees.controller.MainView", {});
+
+        //       onInit: function () {
+
+        //     },
+
+        Main.prototype.onValidate = function () {
+
+            var inputEmployee = this.byId("InputEmployee");
+
+            var valueEmployee = inputEmployee.getValue();
+
+            if (valueEmployee.length === 6) {
+
+                //      inputEmployee.setDescription("valid is ok");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+
+            } else {
+
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
+
+                //      inputEmployee.setDescription("valid is not ok");
+            }
+        };
+
+        return Main;
+    });
